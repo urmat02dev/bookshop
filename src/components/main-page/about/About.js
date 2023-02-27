91e592b70b633eddb586aa7502e7c7f0777f5721
Urmat <urmatzhekshenov@gmail.com>
2023-02-27 18:30:24 +0600

    About starts

--- src/components/main-page/about/About.js
+++ src/components/main-page/about/About.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import "./About.scss"
+import about from "../../../assets/img/фищге.png"
 const About = () => {
   return (
-    <div>
-
+    <div id={"about"}>
+      <div className="container">
+        <div className="about">
+          <div className="about--img">
+            <img src={about} alt=""/>
+          </div>
+          <div className="about--desc">
+            <h1>Best Book Shop In Your Home Town</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
