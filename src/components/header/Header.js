b197ce1cbfcaa5eb91e0e971d9c620f2f98f60a6
myrzakulov <bajelmyrzakulov8@gmail.com>
2023-02-27 18:29:12 +0600

    container

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Header.scss"
+import logo from '../../assets/img/logo.svg'
+import {NavLink} from "react-router-dom";
+import Search from "../pages/search/Search";
+import {BsSearch} from "react-icons/bs";
 const Header = () => {
+    const [count, setCount] = useState(false)
   return (
-    <div>
-      <h1>Header</h1>
+    <div id='header'>
+        <div className="container">
+            <div className="header">
+                <div className="header--start">
+                    <img src={logo} alt=""/>
+                </div>
+                <div className="header--center">
+                    <div className="header--center__start" onClick={() => setCount(true)}>
+                        <button>Search <BsSearch/></button>
+                    </div> 
+                    <div className="header--center__block">
+                        <NavLink to='/catalog'>Catalog</NavLink>
+                        <NavLink to='/payments'>Payments</NavLink>
+                        <NavLink to='/delivery'>Delivery</NavLink>
+                        <NavLink to='/basket'>Basket</NavLink>
+                        <NavLink to='/contacts'>Contacts</NavLink>
+                    </div>
+                    <div className="header--center__end">
+                       <div className="header--center__end--one">
+                           <select>
+                               <option>ENG</option>
+                               <orption>RUS</orption>
+                               <option>KYR</option>
+                           </select>
+                       </div>
+                       <div className="header--center__end--two">
+                           <div>maik</div>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
