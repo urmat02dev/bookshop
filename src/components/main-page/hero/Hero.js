72f2c4dd5b2191a067e8cfa8d4cacd881006d11e
NOTNIK_KG <nasirovasylai@gmail.com>
2023-03-01 13:41:56 +0600

    init

--- src/components/main-page/hero/Hero.js
+++ src/components/main-page/hero/Hero.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import "./Hero.scss"
+import logos from "../../../assets/img/Vector (1).png"
+import books from "../../../assets/img/books.png"
+
 const Hero = () => {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div id="hero">
+            <div className="container">
+                <div className="hero">
+                    <div className="words">
+                        <h1 className="tittle">What Book Are You Looking For? </h1>
+                        <p className="word">Not Sure What To Read Next? Explore Our Catalog</p>
+                    </div>
+
+                    <div className="logo">
+                        <img src={logos} className="font" alt=""/>
+                        <img src={books} className="font2" alt=""/>
+                        <button className="btn">Order Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Hero;
