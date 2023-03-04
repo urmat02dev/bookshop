a5aef9013497801240065a8c7077314bad31a500
myrzakulov <bajelmyrzakulov8@gmail.com>
2023-03-04 16:49:15 +0600

    Added search

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,55 +2,63 @@ import React, {useState} from 'react';
 import "./Header.scss"
 import logo from "../../assets/img/logo.svg"
 import {NavLink} from "react-router-dom";
-import {BsPerson, BsSearch} from "react-icons/bs";
+import {BsSearch} from "react-icons/bs";
 import {useTranslation} from "react-i18next";
 import Search from "../pages/search/Search";
 
 const Header = () => {
-  const [count, setCount] = useState(false)
-  const {t, i18n} = useTranslation()
-  const changeLanguage = (language) => {
-    i18n.changeLanguage(language);
-  };
-   const valueLang = localStorage.getItem("i18nextLng")
-
-
-  return (
-    <div id='header'>
-        <div className="container">
-            <div className="header">
-                <div className="header--start">
-                    <NavLink to={"/"}><img src={logo} alt=""/></NavLink>
-                </div>
-                <div className="header--center">
-                    <div className="header--center__start" onClick={() => setCount(true)}>
-                        <button>Search <BsSearch/></button>
-
-                    </div> 
-                    <div className="header--center__block">
-                        <NavLink to='/catalog'>Catalog</NavLink>
-                        <NavLink to='/payments'>Payments</NavLink>
-                        <NavLink to='/delivery'>Delivery</NavLink>
-                        <NavLink to='/basket'>Basket</NavLink>
-                        <NavLink to='/contacts'>Contacts</NavLink>
+    const [count, setCount] = useState(false)
+    const {t, i18n} = useTranslation()
+    const changeLanguage = (language) =>{
+        i18n.changeLanguage(language);
+    };
+    return (
+        <div id='header'>
+            <div className="container">
+                <div className="header">
+                    <div className="header--start">
+                        <NavLink to={'/'}><img src={logo} alt=""/></NavLink>
                     </div>
-                    <div className="header--center__end">
-                       <div className="header--center__end--one">
-                           <select onChange={(e) => changeLanguage(e.target.value.toLowerCase())} defaultValue={valueLang}>
-                             <option value={"en"}>EN</option>
-                              <option value={"ru"}>RU</option>
-                               <option value={"kg"}>KG</option>
-                           </select>
-                       </div>
-                       <div className="header--center__end--two">
-                            <button><BsPerson className={"header--center__end--two--icon"}/></button>
-                       </div>
+                    <div className="header--center">
+                        <div className="header--center__close" onClick={() => setCount(false)} style={{
+                            display: count ? "block" : "none"
+                        }}>
+
+                        </div>
+                        <div className="header--center__start"onClick={() => setCount(true)}>
+                            <button onClick={() => setCount(true)}>Search <BsSearch/></button>
+                            <div style={{
+                                display: count ? 'block' : 'none'
+                            }}>
+                                <Search/>
+                            </div>
+
+                        </div>
+                        <div className="header--center__block">
+                            <NavLink to='/catalog'>Catalog</NavLink>
+                            <NavLink to='/payments'>Payments</NavLink>
+                            <NavLink to='/delivery'>Delivery</NavLink>
+                            <NavLink to='/basket'>Basket</NavLink>
+                            <NavLink to='/contacts'>Contacts</NavLink>
+                        </div>
+                        <div className="header--center__end">
+                            <div className="header--center__end--one">
+                                <select onChange={(e) => changeLanguage(e.target.value.toLowerCase())}>
+                                    <option>EN</option>
+                                    <option>RU</option>
+                                    <option>KG</option>
+                                </select>
+                            </div>
+                            <div className="header--center__end--two">
+                                <div>Mike</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default Header;
+
