9c2262b7c0c86184d972304c7d22629968957b14
NOTNIK_KG <nasirovasylai@gmail.com>
2023-03-13 23:43:53 +0600

    m

--- src/components/pages/delivery/Delivery.js
+++ src/components/pages/delivery/Delivery.js
@@ -15,22 +15,37 @@ const Delivery = () => {
                    <h1>{t("delivery.h1")}</h1>
                     <div className="block">
                         <div className="basket1">
-                            <h2><SlBasket className="icon"/> {t("delivery.h2")}</h2>
+                           <div className="blocks">
+                               <SlBasket className="icon"/>
+                               <h2> {t("delivery.h2")}</h2>
+                           </div>
                             <p className="text">{t("delivery.p")}</p>
                         </div>
                         <div className="basket2">
-                            <h2><BsTruck className="icon"/> {t("delivery.h2-2")}</h2>
+                            <div className="blocks">
+                                <BsTruck className="icon"/>
+                                <h2>{t("delivery.h2-2")}</h2>
+                            </div>
+
                             <p className="text2">{t("delivery.p-2")}</p>
 
                         </div>
                     </div>
                     <div className="block2">
                         <div className="basket3">
-                            <h2><TbPlayerTrackNext className="icon"/> {t("delivery.h2-3")}</h2>
+                            <div className="blocks">
+                                <TbPlayerTrackNext className="icon"/>
+                                <h2>{t("delivery.h2-3")}</h2>
+                            </div>
+
                             <p className="text3">{t("delivery.p-3")}</p>
                         </div>
                         <div className="basket4">
-                            <h2><AiOutlineMail className="icon"/>{t("delivery.h2-4")}</h2>
+                            <div className="blocks">
+                                <AiOutlineMail className="icon"/>
+                                <h2>{t("delivery.h2-4")}</h2>
+                            </div>
+
                             <p className="text4"> {t("delivery.p-4")}</p>
                         </div>
                     </div>
